32e745dc8c7c69edfb30d7ac7e9a2035c3bdc247
stefanmitrano <stefanmitrano01@gmail.com>
2019-06-19 23:07:15 -0400

    enter

--- express/public/javascripts/login.js
+++ express/public/javascripts/login.js
@@ -1,22 +1,82 @@
-/* Login path*/
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username == "Formget" && password == "formget#123") {
-        alert("Login successfully");
-        window.location = "success.html"; // Redirecting to other page.
-        return false;
-    } else {
-        attempt--; // Decrementing by one.
-        alert("You have left " + attempt + " attempt;");
-        // Disabling fields after 3 attempts.
-        if (attempt == 0) {
-            document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("submit").disabled = true;
-            return false;
+getEntries();
+
+function getEntries() {
+
+    $.ajax({
+        //url: "https://gdata.youtube.com/feeds/api/playlists/PL48A83AD3506C9D36?v=2&alt=json",
+        url: "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PL48A83AD3506C9D36&key=AIzaSyAyvig5VkfPt_lBR4sFl-ajsULtgUHmTwA",
+        dataType: "jsonp",
+
+        success: function(data) {
+            var video_id = "";
+            $.each(data.items, function(i, item) {
+                video_id = video_id + item.snippet.resourceId.videoId + ',';
+            });
+            video_id = video_id.substring(0, video_id.length - 1);
+            console.log(video_id);
+            $.ajax({
+                url: "https://www.googleapis.com/youtube/v3/videos?part=contentDetails%2Cstatistics%2Csnippet%2Cplayer&id=" + video_id + "&key=AIzaSyAyvig5VkfPt_lBR4sFl-ajsULtgUHmTwA",
+                dataType: "jsonp",
+
+                success: function(data) {
+                    $.each(data.items, function(i, item) {
+                        var title = item.snippet.title;
+                        var thumb = item.snippet.thumbnails.default.url;
+                        var numLikes = item.statistics.likeCount;
+                        var numDislikes = item.statistics.dislikeCount;
+                        var viewCount = item.statistics.viewCount;
+
+                        $('#entries tbody').append("<tr><td id='editinplace'>" + title + "</td> <td><img alt='" + title + "' src='" + thumb + "'></img></td><td>" + numLikes + "</td><td>" + numDislikes + "</td><td>" + viewCount + "</td>" + "<td style='display: none' id='delete'><input type='button' class='del' value='Delete'/></td>" + "</tr>");
+                    });
+                    $("table").trigger("update");
+                    setEditInPlace();
+                }
+            });
+
+            $("table").trigger("update");
+            setEditInPlace();
         }
-    }
+    });
+
 }
+
+setEditInPlace();
+
+function setEditInPlace() {
+    $("td").each(function() {
+        var id = $(this).attr("id");
+        if (id == 'editinplace') {
+            $(this).editInPlace({
+                callback: function(unused, enteredText) {
+                    return enteredText;
+                },
+                show_buttons: true
+            });
+        }
+    });
+}
+$(document).ready(function() {
+
+    $("#entries").tablesorter();
+
+    $("#edit").click(function() {
+        var text = $("#edit").val();
+        if (text == 'edit') {
+            $("#edit").val('Close it');
+            $("td").each(function() {
+                var id = $(this).attr("id");
+                if (id == 'delete') $(this).show('slow');
+            });
+        } else {
+            $("#edit").val('edit');
+            $("td").each(function() {
+                var id = $(this).attr("id");
+                if (id == 'delete') $(this).hide('slow');
+            });
+        }
+    });
+    $('.del').live('click', function() {
+        $(this).parent().parent().remove();
+        $("#entries").trigger("update");
+    });
+});
--- twitch.js
+++ twitch.js
@@ -3,8 +3,10 @@ var url = require('url');
 var https = require('https');
 
 //this function will be used mostly for twitch api
+//Makes a get request to the following url through https protocol.
 exports.getSSL=function(Url, clientAdd=true){
- 
+  //clientAdd is a boolean should we add our client_id at the end of the url.
+ //the client_id is a requirement by twitch to identify who is making a request to their API
 
     Url=url.parse(Url,true);
     var qdata=Url.query;
@@ -42,10 +44,47 @@ req.end();
 
 }
 
+exports.postSSL=function(Url,data,callback)
+{
+    Url=url.parse(Url,true);
+var options = {
+  host: Url.host,
+  port: 443,
+  path: Url.pathname+Url.search,
+  method: 'POST',
+   headers: {
+       'Content-Type': 'application/json',
+       'Content-Length': data.length
+     }
+     
+};
+console.log("requesting resource:"+Url.host+Url.pathname+":443");
+var req = https.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+      console.log(chunk);
+      if(typeof(callback)=="function")
+      {
+        var ret=callback(chunk,res.statusCode)
+          if(ret!=undefined){
+            return ret;
+          }
+      }
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+req.write(data);
+req.end();
+}
 
 //this function will verify the OAuth token is valid
-exports.validateOAuth=function(oAuth,userName){
-var ret=false;//ret is short for the return value
+exports.validateOAuth=function(oAuth,callback){
 var options = {
   host: 'id.twitch.tv',
   port: 443,
@@ -55,16 +94,19 @@ var options = {
       'Authorization':'OAuth '+oAuth
   }
 };
-
+console.log("requesting resource:https://id.twitch.tv/oauth2/validate:443");
 var req = https.request(options, function(res) {
   console.log('STATUS: ' + res.statusCode);
   console.log('HEADERS: ' + JSON.stringify(res.headers));
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-      response=JSON.parse(chunk);
-      if(userName==response["login"])
-      {
-          ret=true;
+      console.log(chunk);
+      if(typeof(callback)=="function"){
+          
+          var ret=callback(chunk,res.statusCode)
+          if(ret!=undefined){
+            return ret;
+          }
       }
   });
 });
@@ -74,5 +116,5 @@ req.on('error', function(e) {
 });
 
 req.end();
-return ret;
+
 }
