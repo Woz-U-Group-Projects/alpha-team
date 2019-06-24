dbe481c985b205389362197dfa6d6fac6082b320
stefanmitrano <stefanmitrano01@gmail.com>
2019-06-23 21:42:02 -0400

    enter

---
+++ public/javascripts/script.js
@@ -0,0 +1,28 @@
+ // Load the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+ tag.src = "https://www.youtube.com/player_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+ // Replace the 'ytplayer' element with an <iframe> and
+ // YouTube player after the API code downloads.
+ var player;
+ function onYouTubePlayerAPIReady() {
+   player = new YT.Player('ytplayer', {
+     height: 'auto',
+     width: '100%',	
+                            
+     playerVars:{
+         enablejsapi: 1,
+ listType:'playlist',
+         list: 'PLDr_4M2flXJlRLzruodOncrsz33omi3zp', //put playlist ID HERE <-----------------
+         autoplay: 1,
+       controls: 0,
+       loop: 1,
+       cc_load_policy: 1,	//this fails to work . . . 
+       cc_lang_pref: 'en',
+       iv_load_policy: 3,			     
+     }
+     
+   });			    
+ }
