6942b37400f1a7d68e8e7032a65958cf7b0b6485
Steve Menkin <stevemenkin409@gmail.com>
2019-07-15 23:46:30 -0500

    FRONT END

---
+++ React App - Copy/front-end/my-app/src/indexLoggedIn.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles.module.css';
+
+
+class Car extends React.Component {
+  render() {
+    return <h2>Music Queue</h2>;
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 className={styles.musicPlayerText}>Music Player</h1>
+      <button className={styles.playButton}>Play  </button>
+      <button className={styles.pauseButton}>Pause  </button>
+      <button className={styles.stopButton}>Stop  </button>
+      <button className={styles.deleteButton}>Delete </button>
+      <Car />
+      </div>
+    );
+  }
+}
+
+class Tower extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2 className={styles.title}>Alpha Queue</h2>
+      <a href="http://localhost/api/login?request=twitchauthlink" className={styles.loginButton}>Welcome</a>
+      <Garage />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Tower />, document.getElementById('root'));
