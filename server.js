202bbdef49ff553b7085b58835c0d8a2b0cf3f55
jkken502 <31030116+jkken502@users.noreply.github.com>
2019-07-16 15:50:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,15 +18,6 @@ if(process.env.disable_protection=="true"){
   console.error("WARNING: environment variable disable_protection is enabled, this will grant all users full admin rights!!\nThis is not to be used in production.")
 }
 //end of modules
-//react specific imports
-import cors from "cors"
-import React from "react"
-import { renderToString } from "react-dom/server"
-import { StaticRouter, matchPath } from "react-router-dom"
-import serialize from "serialize-javascript"
-import App from '../shared/App'
-import routes from '../shared/routes'
-//end of react imports
 cookie.start(app);//initializes cookies functionality within express
 //twitch.getSSL("https://api.twitch.tv/kraken/users/44322889"); // was an example of using twitch's api
 //app.use(cors())
@@ -76,50 +67,4 @@ let getdoc;
 
  
 }).listen(80);//binds to port 8080 and listens for incoming requests to the server.
-app.get( /^((?!api\/?$)(?!\/index.html)((?!api\/)).)*/,function ( req, res ) {
-  console.log(process.env.react);
-  if(process.env.react=="true"||process.env.react==true){
-    console.log("this should run.")
-  const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
-  const promise = activeRoute.fetchInitialData
-    ? activeRoute.fetchInitialData(req.header("Cookie"),req.query)
-    : Promise.resolve()
-
-  promise.then((data) => {
-    const context = { data  }
-    const markup = renderToString(
-      <StaticRouter  location={req.url} context={context}>
-        <App />
-      </StaticRouter>
-    )
-
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-          <title>SSR with RR</title>
-          <script src="/bundle.js" defer></script>
-          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
-          
-        </head>
-
-        <body>
-          <div id="app">${markup}</div>
-          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        </body>
-      </html>
-    `)
-  })
-}
-else{
-  console.log("this is not supposted to run.")
-  var q = url.parse(req.url, true);
-  res.sendfile(path.join("./src","/browser",q.pathname));
-}
-  });
  
